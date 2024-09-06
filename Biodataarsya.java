f57bacdef27b7995b02e39f7ae25f0227cb3031f
Arsya-Fisa <Arsya Fikri S>
2024-09-06 16:18:42 +0700

    arsya

---
+++ Biodataarsya.java
@@ -0,0 +1,10 @@
+public class Biodataarsya {
+    public static void main(String[] args) {
+        System.out.println("----- BIODATAKU -----");
+        System.out.print("\nNama    : Arsya Fikri Sabilillah");
+        System.out.print("\nKelas   : SIB 1A");
+        System.out.print("\nNim     : 244107060121");
+        System.out.print("\nAsal    : Blitar");
+        System.out.print("\nAlamat  : Jatimulyo");
+    }
+}
